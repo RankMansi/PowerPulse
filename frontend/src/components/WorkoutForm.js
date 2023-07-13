c97611c2afccd7c90b9e458941dc46068fcfa275
Mansi Rank <109585320+RankMansi@users.noreply.github.com>
2023-07-13 17:42:27 +0530

    Update WorkoutForm.js

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -71,6 +71,7 @@ const WorkoutForm = () => {
             />
 
             <button>Add Workout</button>
+// Error Handling
             {error && <div className="error">{error}</div>}
 
         </form>
